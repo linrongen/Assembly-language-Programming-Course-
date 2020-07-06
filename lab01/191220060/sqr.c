5a6f177c900e7fe396c0b2c3f0c6aedf2ea7252a
191220060 <1792897495@qq.com>
2020-07-06 16:02:28 +0800

    lab01

--- lab01/191220060/sqr.c
+++ lab01/191220060/sqr.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int i,j;
+    long long i,j;
     i=40000;
     j=i*i;
-    printf("The 40000*40000 is %d\n", j);
+    printf("The 40000*40000 is %lld\n", j);
     i=50000;
     j=i*i;
-    printf("The 50000*50000 is %d\n", j);
+    printf("The 50000*50000 is %lld\n", j);
     return 0;
 }
---
+++ lab01/191220060/sqr2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    i=46341;
+    j=i*i;
+    printf("The 40000*40000 is %d\n", j);
+    i=46340;
+    j=i*i;
+    printf("The 50000*50000 is %d\n", j);
+    return 0;
+}
