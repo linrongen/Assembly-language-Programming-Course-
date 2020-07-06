46a2d84717132011f406a3b4c5bb37b9490ef5e1
191220060 <1792897495@qq.com>
2020-07-06 12:53:20 +0800

    lab01

---
+++ lab01/191220060/sqr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    i=40000;
+    j=i*i;
+    printf("The 40000*40000 is %d\n", j);
+    i=50000;
+    j=i*i;
+    printf("The 50000*50000 is %d\n", j);
+    return 0;
+}
